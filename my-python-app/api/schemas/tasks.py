48db6b82fca2e4f94c8674d5af06e623253d11e2
yoichi-takayanagi <yoichi-takayangi@garage.co.jp>
2024-09-30 09:49:06 +0900

    スキーマ途中push

--- my-python-app/api/schemas/tasks.py
+++ my-python-app/api/schemas/tasks.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel, Field
 
-
-class Task(BaseModel):
-    id: int
+class TaskBase(BaseModel):
     title: str | None = Field(None, example="○○をする")
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+    class Config:
+        orm_mode = True
+
+class TaskGetResponse(TaskBase):
+    id: int
     done: bool = Field(False, description="完了フラグ")
+    class Config:
+        orm_mode = True
+
 
 
 # 古いPythonだとパイプが使えないので、Optional[str]と書くらしい
